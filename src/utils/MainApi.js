1c23ba7246e465d6241499f21fd19d5202871231
Novik Yaroslav <yaroslavnowik@yandex.ru>
2023-06-26 10:50:30 +0300

    build

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -2,6 +2,7 @@ import { saveJwt, getJwt } from './localStorageHandler';
 
 const BASE_URL = 'https://api.movies.novik.nomoredomains.monster';
 // URL for local testing 'http://localhost:3001'
+// Main API on 62.84.125.69
 
 function prepareHeaders() {
   const jwt = getJwt();
